97f9804ccd01991ca357cd9cedcd1b41265ea86b
jessica.ferreira <jessica.ferreira@userede.com.br>
2017-11-02 20:46:52 -0200

    aula 6

--- connecteddin/perfis/views.py
+++ connecteddin/perfis/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from perfis.models import Perfil	
+from perfis.models import Perfil
 
 def index(request):
 	#return render(request, 'index.html')
@@ -12,3 +12,13 @@ def exibir(request, perfil_id):
 	perfil = Perfil.objects.get(id=perfil_id)
 
 	return render(request, 'perfil.html', {"perfil":perfil})
+
+def convidar(request, perfil_id):
+	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+	perfil_logado = get_perfil_logado(request)
+	perfil_logado.convidar(perfil_a_convidar)
+	return render(request, 'index.html', {"perfis":Perfil.objects.all()})
+
+
+def get_perfil_logado(request):
+	return Perfil.objects.get(id=1)
